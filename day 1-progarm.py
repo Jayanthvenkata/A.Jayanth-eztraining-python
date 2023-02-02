e930cae6ae24208f74094e04c48232d110d902ac
ABBARAJU VENKATA NAGA SAI JAYANTH <113752293+Jayanthvenkata@users.noreply.github.com>
2023-02-02 19:10:43 +0530

    Add files via upload

---
+++ day 1-progarm.py
@@ -0,0 +1,102 @@
+#additiopn of two numbers 
+a,b=input("Enter").split(".")
+print(a)
+print(b)
+print(a+b)
+
+a,b,str=int(input("Enter")),int(input("Enter")),"hello"
+print(a)
+print(b)
+print(str)
+print(a+b)
+
+
+a,b,c=3,2,32.3
+print(a)
+print(b)
+print(c)
+print(a+b)
+print(b+c)
+print(c+a)
+print(a-b)
+print(b-c)
+print(c-a)
+
+
+a,b=input("Enter").split(".")
+print(a)
+print(b)
+print(int(a)+int(b))
+
+
+a=10
+b=20
+temp=b
+b=a
+a=temp
+print(a,b)
+#swap the numbers
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+print("Before swapping")
+print(a)
+print(b)
+print("After swapping")
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+# area of a rectangular and perimeter
+l=int(input("Enter a length"))
+b=int(input("Enter a breadth"))
+print("area of a rectangle",l*b)
+print("perimeter",2*(l+b))
+
+
+#sugar cane 
+kumar=int(input("no of kgs given by the kumar"))
+sam=kumar/2
+kumar=kumar/2
+kumar=kumar+(sam/2)
+sam=sam/2
+print("left by cane",kumar)
+print("half have sam",sam)
+print("total cane",kumar+sam)
+
+
+x=int(input("no of times"))
+y=x*3
+z=y+56.19
+a=z-10
+print("the final result",a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
